f0c405e00ca51720fc529c94266104d530b5e54b
jainaditi19 <aditijain1719@gmail.com>
2025-05-25 16:54:21 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-tabs-multitask-app/',
    optimizeDeps: {
     include: ['redux-thunk'],
   },
